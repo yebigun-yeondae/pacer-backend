db00729fc435b6397fd80b23d208f3db840597f8
YongJun Cho <whdydwns73@naver.com>
2026-04-08 19:23:31 +0900

    init: spring boot project

---
+++ src/test/java/kr/io/pacer/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.io.pacer.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
