9b23e0e6b78d25c30929b480e890ae51d9377df4
YongJun Cho <whdydwns73@naver.com>
2026-05-17 18:18:13 +0900

    Refactor: GoogleAuthService RestClient 인스턴스 재사용 (KakaoAuthService와 동일한 문제)

--- backend/src/main/java/kr/io/pacer/core/service/GoogleAuthService.java
+++ backend/src/main/java/kr/io/pacer/core/service/GoogleAuthService.java
@@ -1,20 +1,19 @@
 package kr.io.pacer.core.service;
 
 import kr.io.pacer.core.dto.oauth2.GoogleProfileDto;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 public class GoogleAuthService {
+
+    private final RestClient restClient = RestClient.create();
 
     public GoogleProfileDto getProfile(String accessToken) {
         log.info("[Google] 프로필 조회 요청 | token prefix={}", accessToken.substring(0, Math.min(10, accessToken.length())));
-        return RestClient.create()
-                .get()
+        return restClient.get()
                 .uri("https://www.googleapis.com/oauth2/v3/userinfo")
                 .header("Authorization", "Bearer " + accessToken)
                 .retrieve()
